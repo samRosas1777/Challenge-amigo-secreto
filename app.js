07575307e7f8dbb2424bdac4057309aa17f9478b
Sam Rosas <sam78roga@gmail.com>
2025-03-17 16:20:47 -0600

    Se agregar la función de agregarAmigo para el boton añadir

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+var amigos = [];
+let ListaAmigos = document.getElementById("listaAmigos");
+
+function agregarAmigo(){
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    if (nombre) {
+     amigos.push(nombre);
+     ListaAmigos.innerHTML = "";
+     amigos.forEach(nombre => {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        ListaAmigos.appendChild(li);
+     });
+     input.value = "";
+     input.focus();   
+    }else{
+        alert("Por favor inserte un nombre válido");
+    }
+}
